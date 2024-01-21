1ad3f64cf45d88eec48c844c4d35f07333c77375
alexxuss1996 <alekseymelnych132@gmail.com>
2024-01-21 18:25:50 +0200

    test: add test for lexer

---
+++ monkey/lexer/lexer_test.go
@@ -0,0 +1,43 @@
+package lexer
+
+import (
+	"monkey/token"
+	"testing"
+)
+
+// TestNextToken is a function to test the NextToken method of the lexer.
+//
+// It tests the NextToken method using a set of input and expected token types and literals.
+func TestNextToken(t *testing.T) {
+	var input string = "=+(){},;#"
+
+	var tests = []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.COMMENT, "#"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
